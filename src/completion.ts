45a3ee6fb29bcb20ac3afe8cdb69373f666f6a25
温伟煌 <wenweihuang@MacBook-Pro-3.Emap-intl.net>
2020-07-24 17:57:23 +0800

    :bug: fix the complete in `import` statement

--- src/completion.ts
+++ src/completion.ts
@@ -260,19 +260,28 @@ class DictionaryCompletionItemProvider implements vscode.CompletionItemProvider 
                 return this.completeByFirstLetter(firstLetter, addSpace);
             case "javascript":
             case "typescript":
-                //// Inline comment or string
-                const tmpTextBeforeJs = textBefore.replace(/((?<!\\)'|(?<!\\)").*?\1/g, '');
-                if (
-                    /\/{2,}/.test(tmpTextBeforeJs)
-                    || /(?<!\\)'/.test(tmpTextBeforeJs)
-                    || /(?<!\\)"/.test(tmpTextBeforeJs)
-                ) {
-                    return this.completeByFirstLetter(firstLetter, addSpace);
-                }
+
+
                 //// Multiline comment
                 if (/\/\*((?!\*\/)[\W\w])*$/.test(docTextBefore)) {
                     return this.completeByFirstLetter(firstLetter, addSpace);
                 }
+                //// Inline comment or string
+                const tmpTextBeforeJs = textBefore.replace(/((?<!\\)'|(?<!\\)").*?\1/g, '');
+                //// <don't complete in 'import' and 'require'>
+                if (tmpTextBeforeJs.includes('import') || tmpTextBeforeJs.includes('require')) {
+                    return [];
+                }
+
+                if (
+                    /\/{2,}/.test(tmpTextBeforeJs)
+                    || /(?<!\\)'/.test(tmpTextBeforeJs)
+                    || /(?<!\\)"/.test(tmpTextBeforeJs)
+
+                ) {
+                    return this.completeByFirstLetter(firstLetter, addSpace);
+                }
+
                 return [];
             case "python":
                 //// Multiline comment (This check should go before inline comment/string check)
